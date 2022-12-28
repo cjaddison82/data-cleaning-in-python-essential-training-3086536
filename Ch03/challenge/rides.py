97d9a9819550376de27f895fa3cb9479d2794657
cjaddison82 <82113275+cjaddison82@users.noreply.github.com>
2022-12-28 19:37:47 +0000

    //message cannot be NULL

--- Ch03/challenge/rides.py
+++ Ch03/challenge/rides.py
@@ -8,3 +8,10 @@ df
 # - Missing values are not allowed
 # - A plate must be a combination of at least 3 upper case letters or digits
 # - Distance much be bigger than 0
+# %%
+df2 = df.isnull().any(axis=1)
+df2
+# %%
+df3 = df['distance'] > 0
+df3
+# %%
